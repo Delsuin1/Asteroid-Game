4cb570ee6d589f3f8e73347e66819c4c39485418
Delsuin <fnhopyt@gmail.com>
2026-07-22 15:30:09 -0400

    Feature: Moveable player character

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
     while True:
         log_state()
-        for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 
